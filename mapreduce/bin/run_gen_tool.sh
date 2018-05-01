6f133cd0074167450d37529ec9a963402d09ccaf
David Streever <david@streever.com>
2018-04-30 22:33:43 -0400

    scripts for mr gen.

--- mapreduce/bin/run_gen_tool.sh
+++ mapreduce/bin/run_gen_tool.sh
@@ -11,7 +11,7 @@ pushd $APP_DIR
 #. ./hadoop_classpath.sh
 
 APP_JAR=../target/mr-data-utility-3.0-SNAPSHOT-shaded.jar
-MAIN=com.streever.iot.mapreduce.data.utility.mapreduce.DataGenTool
+MAIN=com.streever.iot.mapreduce.data.utility.DataGenTool
 
 #hadoop jar $APP_JAR $MAIN --libjars $GENJARS -json.cfg $1 -count $2 -mappers $3 -output $4
 #hadoop jar $APP_JAR $MAIN -jsonCfg $1 -count $2 -mappers $3 -output $4
