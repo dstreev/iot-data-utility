701bc91370ac390707b66d544de564b30f22a915
David Streever <david@streever.com>
2018-06-26 19:42:29 +0200

    added support for timestamp and incrementing timeseries.

--- data-utility-generator/src/main/java/com/streever/iot/data/utility/generator/fields/DateField.java
+++ data-utility-generator/src/main/java/com/streever/iot/data/utility/generator/fields/DateField.java
@@ -4,28 +4,32 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.streever.iot.data.utility.generator.fields.support.Range;
 import com.streever.iot.data.utility.generator.fields.support.StartStopState;
 
+import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-@JsonIgnoreProperties({"df", "diff", "startStop", "lastIssued"})
+@JsonIgnoreProperties({"df", "startStop", "lastIssued"})
 public class DateField extends FieldBase<String> {
-    private Range<Date> range;
-    private Long diff;
+    private Range<Timestamp> range;
+    private Long diff = 1000l;
     private Long startStopSpan = 100000l;
     private String format = "yyyy-MM-dd HH:mm:ss";
     private DateFormat df = new SimpleDateFormat(format);
+    private Boolean increment = Boolean.FALSE;
     private Boolean current = Boolean.TRUE;
     //private Boolean startStop = Boolean.FALSE;
     private Long lastIssued;
 
-    public Range<Date> getRange() {
+    public Range<Timestamp> getRange() {
         return range;
     }
 
-    public void setRange(Range<Date> range) {
+    public void setRange(Range<Timestamp> range) {
         this.range = range;
-        this.diff = range.getMax().getTime() - range.getMin().getTime();
+        if (!increment) {
+            this.diff = range.getMax().getTime() - range.getMin().getTime();
+        }
         // When Range is set, don't need current.
         this.current = Boolean.FALSE;
     }
@@ -47,10 +51,36 @@ public class DateField extends FieldBase<String> {
         df = new SimpleDateFormat(this.format);
     }
 
+    public Long getDiff() {
+        return diff;
+    }
+
+    public void setDiff(Long diff) {
+        this.diff = diff;
+    }
+
+    public Boolean getIncrement() {
+        return increment;
+    }
+
+    public void setIncrement(Boolean increment) {
+        this.increment = increment;
+    }
+
     @Override
     public String getNext() {
         if (current) {
             return df.format(new Date());
+        } else if (increment) {
+            if (range != null && lastIssued == null) {
+                lastIssued = range.getMin().getTime();
+            } else {
+                double multiplierD = randomizer.nextDouble();
+                long incrementL = Math.round((Long) diff * multiplierD);
+//                System.out.println("Increment: " + incrementL + "Multiplier: " + multiplierD);
+                lastIssued = lastIssued + incrementL;
+            }
+            return df.format(new Date(lastIssued));
         } else {
             Long dateValue = null;
             if (getStartStopState().equals(StartStopState.START)) {
